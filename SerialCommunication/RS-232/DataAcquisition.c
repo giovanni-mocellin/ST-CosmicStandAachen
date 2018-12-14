01d69dbc07ecb21fd9edd35df1d1bb669209cf60
Giovanni Mocellin <gio.moc@gmail.com>
2018-12-14 16:35:54 +0100

    Other mods

--- SerialCommunication/RS-232/DataAcquisition.c
+++ SerialCommunication/RS-232/DataAcquisition.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <iostream>
+#include <fstream>
 #include "rs232.h"
 
 using namespace std;
@@ -8,38 +10,34 @@ using namespace std;
 int main(int cport_nr)
 {
   int i, n;
-  int bdrate=2000000;       /* 2000000 baud */
-
+  int baud = 2000000;
+  
   unsigned char buf[4096];
-
+  
   char mode[]={'8','N','1',0};
   
-  if(RS232_OpenComport(cport_nr, bdrate, mode))
+  if(RS232_OpenComport(cport_nr, baud, mode))
   {
     printf("Can not open comport\n");
     return(0);
   }
-
-  while(1)
+  
+  ofstream outfile;
+  outfile.open ("example.txt");
+  outfile << "Writing this to a file.\n";
+  outfile.close();
+  
+  while (1)
   {
     n = RS232_PollComport(cport_nr, buf, 4095);
-
-    if(n > 0)
+    
+    if (n > 0)
     {
-      buf[n] = 0;   /* always put a "null" at the end of a string! */
-
-      for(i=0; i < n; i++)
-      {
-        if(buf[i] < 32)  /* replace unreadable control-codes by dots */
-        {
-          buf[i] = '.';
-        }
-      }
-
       printf("received %i bytes: %s\n", n, (char *)buf);
     }
   }
-
+  
   return(0);
 }
+
 
